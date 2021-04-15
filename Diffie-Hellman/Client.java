ddf00bfc588a89b721e02993024f6ba8ad4ffeb6
Tejas Pradhan <pradhan.tejas135@gmail.com>
2021-04-15 19:00:41 +0530

    completed D-H key exchange using sockets

--- Diffie-Hellman/Client.java
+++ Diffie-Hellman/Client.java
@@ -1,15 +1,50 @@
+import java.io.*;
 import java.net.Socket;
+import java.util.*;
 
 public class Client {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p, g, Xa;
+        System.out.println("Enter value of p");
+        p = sc.nextInt();
+        System.out.println("Enter value of generator polynomial g");
+        g = sc.nextInt();
+        System.out.println("Enter value of a");
+        Xa = sc.nextInt();
+        int Ya = exp(g, Xa, p);
+        String y = Integer.toString(Ya);
         try {
             System.out.println("Client Program Started");
             Socket soc = new Socket("localhost", 8000);
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+
+            out.println(y);
+            String x = in.readLine();
+            int Xb = Integer.valueOf(x);
+            System.out.println("Public Key received from server : " + Xb);
+
         }
 
         catch (Exception e) {
             System.err.println(e);
         }
 
+    }
+
+    public static int exp(int a, int b, int n) {
+        if (b == 0)
+            return 1;
+
+        else if (b % 2 == 0) {
+            int x = exp(a, b / 2, n);
+            return (x * x) % n;
+        }
+
+        else {
+            int x = exp(a, b / 2, n);
+            return (a * ((x * x) % n)) % n;
+        }
     }
 }
--- Diffie-Hellman/Server.java
+++ Diffie-Hellman/Server.java
@@ -1,17 +1,54 @@
+import java.util.*;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int p, g, Xb;
+        System.out.println("Enter value of p");
+        p = sc.nextInt();
+        System.out.println("Enter value of generator polynomial g");
+        g = sc.nextInt();
+        System.out.println("Enter value of a");
+        Xb = sc.nextInt();
+        int Ya = exp(g, Xb, p);
+        String y = Integer.toString(Ya);
         System.out.println("Waiting for Connections : ");
         try {
             ServerSocket ss = new ServerSocket(8000);
             Socket soc = ss.accept();
             System.out.println("Connection Established");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+            PrintWriter out = new PrintWriter(soc.getOutputStream(), true);
+
+            // reading public key sent by client
+            String x = in.readLine();
+            int Xa = Integer.valueOf(x);
+            System.out.println("Public key received from client : " + Xa);
+            out.println(y);
+
         }
 
         catch (Exception e) {
             System.err.println(e);
+        }
+    }
+
+    public static int exp(int a, int b, int n) {
+        if (b == 0)
+            return 1;
+
+        else if (b % 2 == 0) {
+            int x = exp(a, b / 2, n);
+            return (x * x) % n;
+        }
+
+        else {
+            int x = exp(a, b / 2, n);
+            return (a * ((x * x) % n)) % n;
         }
     }
 }
